22158e4ef0cfb933756406ab3f78ec52e0cb35b3
Jian0512 <eddiexnick@hotmail.com>
2017-03-11 21:03:24 +0800

    加入類別

--- CS280-Homework-2/Program.cs
+++ CS280-Homework-2/Program.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ;
 
 namespace CS280_Homework_2
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
         }
     }
